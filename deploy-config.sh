9fe9370f0d01f546f1a3fcf1559b5250d2a98298
Dima <6072406+go-dima@users.noreply.github.com>
2019-08-05 22:28:06 +0300

    Run remote as default option (#1)
    * Set remote as default script option
    * Update README.md

--- deploy-config.sh
+++ deploy-config.sh
@@ -1,21 +1,28 @@
 #!/bin/bash -e
 
-# clone repo
-if [ ! -z $1 ]; then
-  if [ "$1" == "-remote" ] || [ "$1" == "-r" ]; then
-    git clone https://github.com/go-dima/linux-config.git
-    cd linux-config
+SKIP_CLONE=false
+
+if [[ ! -z "$1" ]]; then
+  if [[ "$1" == "-local" ]] || [[ "$1" == "-l" ]]; then
+    echo Running from local folder
+    SKIP_CLONE=true
   else
     echo Invalid option: $1
     exit 1
   fi
 fi
 
-# clone bash-git-prompt
+if [[ "${SKIP_CLONE}" -eq "false" ]]; then
+  # Clone repo
+  git clone https://github.com/go-dima/linux-config.git
+  cd linux-config
+fi
+
+# Clone bash-git-prompt
 BASH_GIT_PROMPT_URL=https://github.com/magicmonty/bash-git-prompt.git
 BASH_GIT_PROMPT_FOLDER=~/.bash-git-prompt
 
-if [ ! -d "$BASH_GIT_PROMPT_FOLDER" ] ; then
+if [[ ! -d "$BASH_GIT_PROMPT_FOLDER" ]] ; then
     git clone $BASH_GIT_PROMPT_URL $BASH_GIT_PROMPT_FOLDER --depth=1
 else
     cd "$BASH_GIT_PROMPT_FOLDER"
@@ -23,25 +30,26 @@ else
     cd -
 fi
 
-# configure bashrc
+# Configure bashrc
 cat profile/my_bashrc > ~/.bashrc.extra
 PATTERN='source ~/.bashrc.extra'
 BASHRC_FILE=~/.bashrc
 grep -qxF -- "$PATTERN" "$BASHRC_FILE" || echo "$PATTERN" >> "$BASHRC_FILE"
 
-# configure git
+# Configure git
 cat profile/my_gitconfig > ~/.gitconfig
 mkdir -p ~/bin
 cp git-commands/* ~/bin/
 
-# configure vim
+# Configure vim
 cat profile/my_vimrc > ~/.vimrc
 
-# cleanup
-if [ ! -z $1 ]; then
+# Cleanup
+if [[ -z "$1" ]]; then
   cd ..
   rm -rf linux-config
 fi
 
-# apply changes
+# Apply changes
 source ~/.bashrc
+echo Configuration complete
